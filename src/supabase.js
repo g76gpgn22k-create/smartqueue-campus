f9ce8e53d624df3240c7a8462fd680b917087d0e
g76gpgn22k-create <g76gpgn22k@privaterelay.appleid.com>
2026-08-31 00:13:32 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,4 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const url = import.meta.env.VITE_SUPABASE_URL
 const key = import.meta.env.VITE_SUPABASE_ANON_KEY
-export const supabase = createClient(url || 'https://YOUR-PROJECT.supabase.co', key || 'YOUR-ANON-KEY')
+export const supabase = createClient(url, key)
